69a776f95b3eac75b86be91075fa88ffbacc7e4c
Sercantugba <sercantugba834@gmail.com>
2020-07-30 23:54:26 -0500

    continue to working reset function

--- src/App.js
+++ src/App.js
@@ -1,6 +1,12 @@
 import React from 'react';
 import './App.css';
 
+// Objective
+
+// Create a <div> box with an onClick handle event that changes the background color when clicked.
+// When the box is clicked, we want a clickCounter indicating how many times it was clicked. 
+// We want the box color to toggle back to it's original color! (Hint: create a toggle function)
+
 class App extends React.Component {
   constructor(props) {
     super(props);
